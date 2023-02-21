b01632c20225c85f28206bcf8c7beb1e9b74bf37
Joao-MCF <marcosc024@gmail.com>
2023-02-21 15:31:00 -0300

    Update dependencies and remove unused files from project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import { UserProvider } from "./services/UserContext"
+import Header from "./components/header/Header"
+import Home from "./components/feed/Home"
+import PinBuilder from "./components/pin/PinBuilder"
+import PinViewer from "./components/pin/PinViewer"
+import UserProfile from "./components/user/UserProfile"
+import BoardPins from "./components/board/BoardPins"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <UserProvider>
+        <Header />
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/pin-builder" element={<PinBuilder />} />
+          <Route path="/pin/:id" element={<PinViewer />} />
+          <Route path="/:username" element={<UserProfile />} />
+          <Route path="/:username/:board" element={<BoardPins />} />
+        </Routes>
+      </UserProvider>
+    </Router>
+  )
 }
 
-export default App;
+export default App
